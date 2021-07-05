30c371c8bde9528fe388e0a9c022289443b1b02c
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2021-07-05 18:54:31 +0200

    allineamento

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("This is my website")
+window.addEventListener('scroll', (event) => {
+
+    const navBar = document.getElementById("navBar")
+    console.log(navBar.clientHeight);
+    console.log(window.scrollY);
+    console.log("-----------------");
+    if (navBar.clientHeight < window.scrollY) {
+        navBar.classList.add("hidden");
+    } else {
+        navBar.classList.remove("hidden");
+    }
+
+})

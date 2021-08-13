01a3069d69620e77e6ae3ac0e5b0ce49c9582f8a
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2021-08-13 15:31:02 +0200

    added backToTop

--- !script_minified.js
+++ !script_minified.js
@@ -1,14 +1,17 @@
 window.addEventListener('scroll', (event) => {
 
-    const NAVBAR = document.getElementById("navBar");
+    const NAVBAR    = document.getElementById("navBar");
     const NAVIGATOR = document.getElementById("navigator");
+    const BACKTOTOP = document.getElementById("backToTop");
     
     if (NAVBAR.clientHeight < window.scrollY) {
         NAVBAR.classList.add("hidden");
         NAVIGATOR.classList.remove("hidden");
+        BACKTOTOP.classList.remove("hidden");
     } else {
         NAVBAR.classList.remove("hidden");
         NAVIGATOR.classList.add("hidden");
+        BACKTOTOP.classList.add("hidden");
     }
 
 })
@@ -21,4 +24,9 @@ window.onload = function() {
         } 
         
     })
+}
+
+function backToTop() {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
 }

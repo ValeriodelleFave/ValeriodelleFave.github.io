04467e61ee32a213642f60c199f3ec8d7a5033ec
ValeriodelleFave <valerio.dellefave00@gmail.com>
2021-09-08 10:57:47 +0200

    TODO scroll synchronized with navigation bar

--- !script_minified.js
+++ !script_minified.js
@@ -10,3 +10,40 @@ function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function resetActiveLink() {
+    document.getElementById("presentationLink").classList.remove("active");
+    document.getElementById("aboutLink").classList.remove("active");
+    document.getElementById("projectsLink").classList.remove("active");
+    document.getElementById("hobbyLink").classList.remove("active");
+    document.getElementById("contactsLink").classList.remove("active");
+}
+
+function checkScrolling() {
+    let presentationY = document.getElementById("presentation").getBoundingClientRect().y,
+        aboutY        = document.getElementById("about").getBoundingClientRect().y,
+        projectsY     = document.getElementById("projects").getBoundingClientRect().y,
+        hobbyY        = document.getElementById("hobby").getBoundingClientRect().y,
+        contactsY     = document.getElementById("contacts").getBoundingClientRect().y;
+
+    resetActiveLink()
+
+    if (presentationY >= 0 && presentationY <= aboutY) {
+        document.getElementById("presentationLink").classList.add("active");
+    } else if (aboutY >= 0 && aboutY <= projectsY) {
+        document.getElementById("aboutLink").classList.add("active");
+    } else if (projectsY >= 0 && projectsY <= hobbyY) {
+        document.getElementById("projectsLink").classList.add("active");
+    } else if (hobbyY >= 0 && hobbyY <= contactsY) {
+        document.getElementById("hobbyLink").classList.add("active");
+    } else if (contactsY >= 0) {
+        document.getElementById("contactsLink").classList.add("active");
+    } else {
+
+    }
+
+}
+
+
+
+document.addEventListener("scroll", checkScrolling, false);

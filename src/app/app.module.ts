3b0f7ebcc39c259cbb9eb51618f9093d6c95c90d
ValeriodelleFave <valerio.dellefave00@gmail.com>
2021-10-26 21:19:21 +0200

    Revert "(forse) angu"
    This reverts commit 2c8045c1f7bd02ae88b4173739041ffebf6c3be1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { PresentationComponent } from './components/presentation/presentation.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { HobbyComponent } from './components/hobby/hobby.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    PresentationComponent,
-    SkillsComponent,
-    HobbyComponent,
-    ContactsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

b1f299849763fb9f637bc555ae7585d16da41a36
Valerio delle Fave <48824674+ValeriodelleFave@users.noreply.github.com>
2022-08-31 16:26:02 +0200

    Update color-wheel.component.ts

--- src/components/color-wheel/color-wheel.component.ts
+++ src/components/color-wheel/color-wheel.component.ts
@@ -9,6 +9,23 @@ export class ColorWheelComponent implements OnInit {
 
   constructor() { }
 
+  primaryColors = ["giallo", "rosso", "blu"];
+  secondaryColors = ["arancione", "viola", "verde"];
+  tertiaryColors = ["giallo arancione", "rosso arancione", "rosso viola", "blu viola", "blu verde", "verde giallo"];
+
+  hotColors = ["giallo", "giallo arancione", "arancione", "rosso arancione", "rosso", "rosso viola"];
+  coldColors = ["viola", "blu viola", "blu", "blu verde", "verde", "verde giallo"];
+
+  rules = {
+    monocromatici: "",
+    analoghi: "",
+    analoghi_ampliati: "",
+    complementari: "",
+    complementari_divergenti: "",
+    triade: "",
+    quartetto: "",
+  }
+
   ngOnInit(): void {
   }
 
